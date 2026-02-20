82c2604466ab86d308c469725b5d26196333fa35
Cristian Javier Benavides Morales <cristian.benavides1503@gmail.com>
2026-02-19 23:45:43 -0600

    new version in checkplanconditions updated

--- writeback/checkPlanConditions.py
+++ writeback/checkPlanConditions.py
@@ -5,69 +5,140 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from globalVariables.script import ELG_PATTERNS,data_supplies,static_regex,iv_config
 from globalFunctions.script import clean_regex,get_info
 
-elg_carriers_clinic = clean_regex(get_info(iv_config,'elg_carriers_clinic'))
 
-def is_elg_plan(clinics_bot):
-    if clinics_bot in iv_config['elg_clinic']['All_Clinics']:
+class PlanRule:
+
+    def __init__(
+            self,
+            name,
+            carrier_regex = None,
+            verification_include = None,
+            verification_exclude = None
+        ):
+        
+        self.name = name
+        self.carrier_regex = (
+            re.compile(carrier_regex,re.IGNORECASE)
+            if carrier_regex else None
+        )
+        
+        self.verification_include = verification_include or []
+        self.verification_exclude = verification_exclude or []
+
+    
+    def found_plan(self,data_supplies,iv_config,ELG_PATTERNS):
+
+        practice = data_supplies['practice'].lower()
+        allowed_practice = [p.lower() for p in iv_config['elg_clinics'].get(self.name) or []]
+        
+        if "all" not in allowed_practice and practice not in allowed_practice:
+            return False
+        
+        if self.carrier_regex:
+            if not self.carrier_regex.search(data_supplies['carrier_name']):
+                return False
+            
+        for key in self.verification_include:
+            pattern_or_dict = ELG_PATTERNS.get(key)
+            if isinstance(pattern_or_dict, dict):
+                if not any(re.search(pat, data_supplies['verification_status'], re.IGNORECASE)
+                        for pat in pattern_or_dict.values()):
+                    return False
+            else:        
+                if not re.search(pattern_or_dict, data_supplies['verification_status'], re.IGNORECASE):
+                    return False
+
+        
+        for key in self.verification_exclude:
+            pattern_or_dict = ELG_PATTERNS.get(key)
+            if isinstance(pattern_or_dict, dict):
+                if any(re.search(pat, data_supplies['verification_status'], re.IGNORECASE)
+                    for pat in pattern_or_dict.values()):
+                    return False
+            else:
+                if re.search(pattern_or_dict, data_supplies['verification_status'], re.IGNORECASE):
+                    return False
+       
         return True
-    else:
-        return(data_supplies['type_of_verification'] == 'ELG' and
-        data_supplies['practice'] in iv_config['elg_clinic'][clinics_bot] and 
-        bool(re.search(elg_carriers_clinic,data_supplies['carrier_name'])))
 
-def check_plan_conditions():
-    sky_reg = r'^(((United( )?Health( )?Care|UHC|AZUHC)( ?(-?Comm(unity)?)( ?Plan?)?| Delaware)|UHC ?CP.*)|(Delta )?(Dental|Delta).*(KY|Kentucky).*)$'
-    cs_reg = r'^((Horizon )?(NJ )(health)).*$'
-    csea_reg = r'(?i)^(CSEA).*|^(C\.S\.E\.A\.?).*'
-    def check_encore_nj_plan():
-        for plan_key, plan_pattern in ELG_PATTERNS['encore_nj_pattern'].items():
-            if re.search(plan_pattern, data_supplies['verification_status'], re.IGNORECASE):
-                return plan_key  
-        return None 
+
+class PlanEvaluate:
+
+    def __init__(self,plans):
+        self.plans = plans
     
-    dq_matituck_plan = (is_elg_plan("Dentaquest") and 
-    bool(re.search(r'(denta quest|dentaquest)',data_supplies['carrier_name'],re.IGNORECASE))
-    and data_supplies['practice'].lower() == 'mattituck')
-
-    uhccp_middleisland = (bool(re.search(sky_reg,data_supplies['carrier_name'],re.IGNORECASE)) and 
-    data_supplies['practice'].lower() == 'middleisl')
-
-    uhccp_plan = (is_elg_plan("Skygen_NY") and 
-    not bool(re.search(ELG_PATTERNS['dual_pattern'],data_supplies['verification_status'],re.IGNORECASE)) and 
-    not bool(re.search(ELG_PATTERNS['nj_family_pattern'],data_supplies['verification_status'],re.IGNORECASE)) and 
-    bool(re.search(sky_reg,data_supplies['carrier_name'],re.IGNORECASE)))
-
-    uhccp_dual_plan = (is_elg_plan("Skygen_DUALNY") and
-    bool(re.search(ELG_PATTERNS['dual_pattern'], data_supplies['verification_status'], re.IGNORECASE)) and
-    bool(re.search(sky_reg,data_supplies['carrier_name'],re.IGNORECASE)))
-
-    uhccp_nj_plan = (is_elg_plan("Skygen_NJ") and
-    bool(re.search(ELG_PATTERNS['nj_family_pattern'], data_supplies['verification_status'], re.IGNORECASE)) and
-    bool(re.search(sky_reg,data_supplies['carrier_name'],re.IGNORECASE)))
-
-    encore_nj_plan = (is_elg_plan("Caresource") and check_encore_nj_plan() and
-    bool(re.search(cs_reg,data_supplies['carrier_name'],re.IGNORECASE)))
-    print(encore_nj_plan,"ENCORE NJ PLAN")
-
-    cs_all_plan = check_encore_nj_plan() if encore_nj_plan else False
-    csea_all_plan = (bool(re.search(csea_reg,data_supplies['carrier_name'],re.IGNORECASE)))
+    def evaluate(self, data_supplies, iv_config, ELG_PATTERNS):
+        results = {}
+        for plan in self.plans:
+            results[plan.name] = plan.found_plan(
+                data_supplies,
+                iv_config,
+                ELG_PATTERNS
+            ) 
+        return results
     
-    hmo_uhc = True if ("HMO" in data_supplies["verification_status"].upper() and re.search(static_regex["re_uhc"],data_supplies['carrier_name'],re.IGNORECASE) and "view" in data_supplies['urls']) else False
 
-    dq_fishkill_plan = (bool(re.search(static_regex['dentaquest'],data_supplies['carrier_name'],re.IGNORECASE)) and data_supplies['practice'].lower() == 'fishkill')
+plans = [
+    PlanRule(
+        name = 'dq_mattituck_plan',
+        carrier_regex = static_regex['dentaquest'],
+    ),
+    PlanRule(
+        name = 'uhccp_plan',
+        carrier_regex = static_regex['skygen_reg'],
+        verification_exclude=['dual_pattern','nj_family_pattern']
+    ),
+    PlanRule(
+        name = 'uhccp_dual_plan',
+        carrier_regex = static_regex['skygen_reg'],
+        verification_include=['dual_pattern']
+    ),
+    PlanRule(
+        name = 'uhccp_nj_plan',
+        carrier_regex = static_regex['skygen_reg'],
+        verification_include=['nj_family_pattern']
+    ),
+    PlanRule(
+        name = 'caresorce_all_plan',
+        carrier_regex = static_regex['caresorce_reg'],
+        verification_include=["caresource_nj_pattern"]
+    ),
+    PlanRule(
+        name = 'uhccp_middleisland',
+        carrier_regex = static_regex['skygen_reg']     
+    ),
+    PlanRule(
+        name = 'csea_all_plan',
+        carrier_regex = static_regex['csea_reg']     
+    ),
+    PlanRule(
+        name = 'hmo_uhc_plan',
+        carrier_regex = static_regex['re_uhc'],
+        verification_include=["hmo"]     
+    ),
+    PlanRule(
+        name = 'dq_fishkill_plan',
+        carrier_regex = static_regex['dentaquest']
+    ),
+    PlanRule(
+        name = 'dq_catskill_plan',
+        carrier_regex = static_regex['dentaquest']
+    ),
+    PlanRule(
+        name = 'liberty_mattituck_plan',
+        carrier_regex = static_regex['liberty']
+    )                                            
+]    
 
-    elg_plans_flags = {
-        "dq_matituck_plan":dq_matituck_plan,
-        "uhccp_plan":uhccp_plan,
-        "uhccp_dual_plan":uhccp_dual_plan,
-        "uhccp_nj_plan":uhccp_nj_plan,
-        "cs_all_plan": cs_all_plan,
-        'uhc_middleisland_plan' : uhccp_middleisland,
-        'csea_all_plan' : csea_all_plan,
-        'hmo_uhc' : hmo_uhc,
-        'dq_fishkill_plan' : dq_fishkill_plan
-        }
-    print(elg_plans_flags)
-    return elg_plans_flags
 
+engine = PlanEvaluate(plans)
+
+plan_results = engine.evaluate(
+    data_supplies,
+    iv_config,
+    ELG_PATTERNS
+)
+
+print(plan_results)
+true_keys = [key for key, value in plan_results.items() if value]
 
